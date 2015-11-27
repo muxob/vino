e3cb764224dfbbfecb7eca17971970227de2a60f
Iliyan Mihov <ilian.mihov@gmail.com>
2015-11-27 18:30:17 +0200

    registration server - eureka

---
+++ registration-server/src/test/java/com/ilko/vino/RegistrationServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ilko.vino;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RegistrationServerApplication.class)
+public class RegistrationServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
