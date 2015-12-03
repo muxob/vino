84cb3f53d87bbaa3696c5188723156e155c704ae
Iliyan Mihov <ilian.mihov@gmail.com>
2015-12-03 15:22:50 +0200

    add OAuth2 authentication

--- catalog/src/main/java/com/ilko/vino/CatalogApplication.java
+++ catalog/src/main/java/com/ilko/vino/CatalogApplication.java
@@ -2,10 +2,12 @@ package com.ilko.vino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableOAuth2Sso
 public class CatalogApplication {
 
     public static void main(String[] args) {
