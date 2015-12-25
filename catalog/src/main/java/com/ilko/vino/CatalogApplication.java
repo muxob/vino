41dbf9c0845540a432baa35caa3dde61292ff7a0
ilian <ilian.mihov@gmail.com>
2015-12-25 23:15:54 +0200

    config for client-ui

--- catalog/src/main/java/com/ilko/vino/CatalogApplication.java
+++ catalog/src/main/java/com/ilko/vino/CatalogApplication.java
@@ -2,12 +2,12 @@ package com.ilko.vino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableOAuth2Sso
+@EnableResourceServer
 public class CatalogApplication {
 
     public static void main(String[] args) {
